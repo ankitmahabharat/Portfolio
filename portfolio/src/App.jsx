867dee71c966a709f14eeb4d0ec93785fbd17cdb
Ankit Mahabharat <av700600@gmail.com>
2026-01-28 23:26:37 +0530

    Adding sidebar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Sidebar from "./components/Sidebar";
 
 
 function App() {
   return <>
   <Navbar></Navbar>
-  
+  <Sidebar></Sidebar>
+  <Footer></Footer>
   </>
 }
 
