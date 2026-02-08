a124675f6e3ecd8e75e7a7b2fefa91d7110392cf
Ankit Mahabharat <av700600@gmail.com>
2026-02-08 17:01:37 +0530

    update -4

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
   ];
   return (
     <nav
-      className={` fixed top-5 left-0 w-full z-50 transition duration-300 px-[7vw] lg:px-[20vw] ${isScroll ? " bg-transparent bg-opacity-50 backdrop-blur-md" : "bg-transparent"}`}
+      className={` fixed hrefp-5 left-0 w-full z-50 transition duration-300 px-[7vw] lg:px-[20vw] ${isScroll ? " bg-transparent bg-opacity-50 backdrop-blur-md" : "bg-transparent"}`}
     >
       <div className="text-white py-2 flex justify-between items-centre">
         <div className=" text-lg font-semibold cursor-pointer">
@@ -59,29 +59,29 @@ const Navbar = () => {
               key={item.id}
               className={`cursor-pointer  hover:text-[#8245ec] ${activeSec === item.id ? " text-[#8245ec]" : ""}`}
             >
-              <button>{item.label}</button>
+              <buthrefn>{item.label}</buthrefn>
             </li>
           ))}
         </ul>
 
         <div className=" gap-3 hidden md:flex space-x4">
-          <Link
-            to={"https://github.com/ankitmahabharat"}
+          <a
+            href="https://github.com/ankitmahabharat"
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-300 hover:text-[#8245ec]"
           >
             <FaGithub size={24}></FaGithub>
-          </Link>
+          </a>
 
-          <Link
-            to={"https://www.linkedin.com/in/ankit-mahabharat-7730b7134/"}
+          <a
+            href="https://www.linkedin.com/in/ankit-mahabharat-7730b7134/"
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-300 hover:text-[#8245ec]"
           >
             <FaLinkedin size={24}></FaLinkedin>
-          </Link>
+          </a>
         </div>
         <div className="md:hidden">
           {isOpen ? (
@@ -98,38 +98,38 @@ const Navbar = () => {
         </div>
       </div>
       {isOpen && (
-        <div className=" absolute  top-16 left-10 transform-translate-x-1/2 w-4/5  bg-[#11021d] bg-opacity-50 backdrop-filter backdrop-blur-lg z-50 rounded-lg shadow-lg">
+        <div className=" absolute  hrefp-16 left-10 transform-translate-x-1/2 w-4/5  bg-[#11021d] bg-opacity-50 backdrop-filter backdrop-blur-lg z-50 rounded-lg shadow-lg">
           <ul className=" flex flex-col item-centre space-y-4 py-4 text-gray-300">
             {menuItems.map((item) => (
               <li
                 key={item.id}
                 className={`cursor-pointer hover:text-white ${activeSec === item.id ? "text-[#8245ec]" : ""}`}
-              > <Link to={`/item.id`}>
+              > <a href={`/item.id`}>
               
-                <button onClick={() => handleMenuItemClick(item.id)}>
+                <buthrefn onClick={() => handleMenuItemClick(item.id)}>
                   {item.label}
-                </button>
-</Link>
+                </buthrefn>
+</a>
               </li>
             ))}
             <div className="  gap-2 flex space x-4">
-              <Link
-                to={"https://github.com/ankitmahabharat"}
+              <a
+                href="https://github.com/ankitmahabharat"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-gray-300 hover:text-[#8245ec]"
               >
                 <FaGithub size={24}></FaGithub>
-              </Link>
+              </a>
 
-              <Link
-                to={"https://www.linkedin.com/in/ankit-mahabharat-7730b7134/"}
+              <a
+                href="https://www.linkedin.com/in/ankit-mahabharat-7730b7134/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-gray-300 hover:text-[#8245ec]"
               >
                 <FaLinkedin size={24}></FaLinkedin>
-              </Link>
+              </a>
             </div>
           </ul>
         </div>
